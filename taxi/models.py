0953fae9cbcd9a60e344e05bad2f90ae88f9b20a
Fedir Kiiko <fedir1357@gmail.com>
2026-09-21 15:54:10 +0300

    __str__ to driver

--- taxi/models.py
+++ taxi/models.py
@@ -45,3 +45,6 @@ class Driver(AbstractUser):
         ordering = ("username", )
         verbose_name = "driver"
         verbose_name_plural = "drivers"
+
+    def __str__(self) -> str:
+        return f"{self.username}: {self.first_name} {self.last_name}"
