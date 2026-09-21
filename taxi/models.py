3275eb5e4fc441a90e7373edc70a48efce121a66
Fedir Kiiko <fedir1357@gmail.com>
2026-09-21 17:27:41 +0300

    small fix

--- taxi/models.py
+++ taxi/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from taxi_service.settings import AUTH_USER_MODEL
+from django.conf import settings
 
 
 class Manufacturer(models.Model):
@@ -25,7 +25,7 @@ class Car(models.Model):
         on_delete=models.CASCADE
     )
     drivers = models.ManyToManyField(
-        AUTH_USER_MODEL,
+        settings.AUTH_USER_MODEL,
         related_name="cars"
     )
 
